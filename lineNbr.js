bce0982059f3e5e26017d2f09c125ab4c16dbd55
bestape <kyle@bestape.net>
2014-05-27 19:14:00 -0700

    init commit

---
+++ lineNbr.js
@@ -0,0 +1,26 @@
+#! usr/bin/env node
+// Example:
+// require('./lineNbr.js')([ 'On line ', ':' ])
+// console.log(__l, 'snakes are in the grass')
+var pre = ''
+, post = ''
+Object.defineProperty(global, '__stack', {
+  get: function(){
+    var orig = Error.prepareStackTrace;
+    Error.prepareStackTrace = function(_, stack){ return stack; };
+    var err = new Error;
+    Error.captureStackTrace(err, arguments.callee);
+    var stack = err.stack;
+    Error.prepareStackTrace = orig;
+    return stack;
+  }
+})
+Object.defineProperty(global, '__l', {
+  get: function(){
+	  return pre + __stack[1].getLineNumber() + post
+  }
+})
+module.exports = function (d) { 
+	pre = d[0]
+	post = d[1]
+}
